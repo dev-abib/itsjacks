05ccc9d4b18a6878a3953f2b05f3bc93e390b8dc
sultan93291 <abibdipto@gmail.com>
2025-10-15 17:18:32 +0600

    feat: cors file updated

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
-      "https://itsjacks-dashboard.vercel.app/",
+      "https://itsjacks-dashboard.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
