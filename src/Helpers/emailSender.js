17901d4f79e8ea48d5d48de80e327668a1f7c06f
sultan93291 <abibdipto@gmail.com>
2026-02-24 15:52:37 +0600

    feat:email handler functionality upated

--- src/Helpers/emailSender.js
+++ src/Helpers/emailSender.js
@@ -1,6 +1,9 @@
 // Import necessary modules
-const axios = require('axios');
-const { PasswordResetTemplate, AccountVerificationTemplate } = require('./email.template');
+const axios = require("axios");
+const {
+  PasswordResetTemplate,
+  AccountVerificationTemplate,
+} = require("./email.template");
 
 // Async function to send an email using Brevo's API
 const mailSender = async ({ type, name, emailAdress, subject, otp }) => {
@@ -8,17 +11,20 @@ const mailSender = async ({ type, name, emailAdress, subject, otp }) => {
     let html;
 
     // Determine the HTML template based on the email type (otp or verify-account)
-    if (type === 'otp') {
+    if (type === "otp") {
       html = PasswordResetTemplate(name, otp, emailAdress);
     }
 
-    if (type === 'verify-account') {
+    if (type === "verify-account") {
       html = AccountVerificationTemplate(name, otp, emailAdress);
     }
 
     // Prepare the email data for Brevo's API
     const emailData = {
-      sender: { email: process.env.MAIL_FROM_ADDRESS, name: process.env.MAIL_FROM_NAME },
+      sender: {
+        email: process.env.MAIL_FROM_ADDRESS,
+        name: process.env.MAIL_FROM_NAME,
+      },
       to: [{ email: emailAdress }],
       subject: subject,
       htmlContent: html,
@@ -26,24 +32,25 @@ const mailSender = async ({ type, name, emailAdress, subject, otp }) => {
 
     // Send the email using Brevo's API via Axios
     const response = await axios.post(
-      'https://api.brevo.com/v3/smtp/email', 
-      emailData, 
+      "https://api.brevo.com/v3/smtp/email",
+      emailData,
       {
         headers: {
-          'Content-Type': 'application/json',
-          'api-key': process.env.BREVO_API_KEY, 
-        }
+          "Content-Type": "application/json",
+          "api-key": process.env.BREVO_API_KEY,
+        },
       }
     );
 
 
     return response.data;
   } catch (error) {
-
-    console.error('Email sending failed:', error.response ? error.response.data : error.message);
-    throw error;  
+    console.error(
+      "Email sending failed:",
+      error.response ? error.response.data : error.message
+    );
+    throw error;
   }
 };
-
 
 module.exports = { mailSender };
